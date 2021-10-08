b1da501a6cde3a567c8b98f19ed15c8cff88375b
Rskiw <riskinurliana03@gmail.com>
2021-10-09 05:34:13 +0800

    Tugas2

--- lib/main.dart
+++ lib/main.dart
@@ -18,23 +18,31 @@ class MyApp extends StatelessWidget {
 class home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          leading: Icon(Icons.menu),
-          title: Text('Hello Appbar'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search, color: Colors.black),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.more_vert, color: Colors.black),
-              onPressed: () {},
-            ),
-          ],
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        leading: Icon(Icons.menu),
+        title: Text('Hello Appbar'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.black),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        color: Colors.black26,
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        padding: EdgeInsets.all(20),
+        child: Icon(
+          Icons.home,
+          color: Colors.white38,
+          size: 75,
         ),
-        body: Center(
-          child: Text('Riski Nurliana_19710089_Tugas1'),
-        ));
+      ),
+    );
   }
 }
